dc0cf36e9e307290f9a9217b9b6125791bf73a6f
justfungx <justfungx@hotmail.com>
2016-09-20 16:17:36 +0800

    V6?

--- app/src/main/java/tw/org/iii/webview/Page2Activity.java
+++ app/src/main/java/tw/org/iii/webview/Page2Activity.java
@@ -1,22 +1,37 @@
 package tw.org.iii.webview;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class Page2Activity extends AppCompatActivity {
 
     private WebView webview;
-
+    private LocationManager mgr;
+    private MyGPSListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
 
-        webview = (WebView) findViewById(R.id.page2_webview);
+        mgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new MyGPSListener();
+
+        webview = (WebView)findViewById(R.id.page2_webview);
         initWebView();
     }
 
@@ -28,6 +43,66 @@ public class Page2Activity extends AppCompatActivity {
         settings.setJavaScriptEnabled(true);
 
         webview.loadUrl("file:///android_asset/page2.html");
+    }
 
+
+    @Override
+        protected void onResume() {
+            super.onResume();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+
+        }
+        @Override
+        protected void onPause() {
+            super.onPause();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mgr.removeUpdates(listener);
+
+        }
+        private class MyGPSListener implements LocationListener {
+            @Override
+            public void onLocationChanged(Location location) {
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+                webview.loadUrl("javascript:moveMap(" + lat + ", " + lng + ")");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        }
+
+    public void test1(View v){
+        webview.loadUrl("javascript:moveMap(11.146499, 122.516159)");
     }
 }
